a6ef293d21c5b9add65d3da92d28b6d43915757a
JeodC <47716344+JeodC@users.noreply.github.com>
2024-01-19 22:48:25 -0500

    Big huge update
    Does a lot of stuff

---
+++ Hallow's Eve/Hallow's Eve.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Source SDL controls
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+source $controlfolder/control.txt
+get_controls
+
+# Set variables
+GAMEDIR="/$directory/ports/hallowseve"
+CONFIG="/$directory/gamedata/solarus/saves"
+
+#Check for savedir
+if [ ! -d "$CONFIG" ]; then
+    mkdir -p "$CONFIG"
+fi
+
+cd $GAMEDIR
+
+# Patch stuff
+if [ -d "$GAMEDIR/patch/" ]; then
+    for solarus_file in "$GAMEDIR/game"/*.solarus; do
+        if [ -e "$solarus_file" ]; then
+            "$GAMEDIR/lib/7za" l "$solarus_file" | grep -i 'coroutine_helper.lua'
+            if [ $? -eq 0 ]; then
+                "$GAMEDIR/lib/7za" a -r "$solarus_file" "$GAMEDIR/patch/scripts/"
+                rm -r "$GAMEDIR/patch/"
+            fi
+        fi
+    done
+fi
+
+# Setup controls
+$ESUDO chmod 666 /dev/tty1
+$ESUDO chmod 666 /dev/uinput
+$GPTOKEYB "solarus-run" -c "hallowseve.gptk" & 
+SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+
+# Run the game
+./solarus-run $GAMEDIR/game/*.solarus 2>&1 | tee -a ./"log.txt"
+$ESUDO kill -9 $(pidof gptokeyb)
+$ESUDO systemctl restart oga_events & 
+printf "\033c" >> /dev/tty1
--- README.md
+++ README.md
@@ -4,13 +4,13 @@ The Solarus RPG game engine set up to run with PortMaster. Many retro handheld f
 By running Solarus through PortMaster, we can provide additional libraries as needed and customize controls via GPtoKeyB (Gamepad to Keyboard emulation).
 
 ## Usage
-Unzip to ports folder. Scripts provided are an example, and an easy template. For different games, one only needs to change the `$GAME` variable. Games (the `.solarus` files) need to be downloaded separately and added to the `solarus/games` folder. 
+Unzip to ports folder. Games (the `.solarus` files) need to be downloaded separately and added to the `solarus/games` folder. 
 Find them on the [Solarus Website](https://solarus-games.org/games/).  
 
 You can copy the contents of `gamelist.xml` into your own `gamelist.xml` file.
 
 ## Disclaimers
-This is just a Solarus runner. Certain games may have issues running on various devices. The games `Ocean's Heart`, `Hallow's Eve`, and `Tunics!` are a few that do not function as expected on devices using luaJIT.
+Certain games have patches applied on first run. These patches help the games boot and play, but may have unintended side effects later on.
 
 ## Thanks
 The solarus-run binary provided by Cebion.
